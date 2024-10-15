f110c295847e11f01bd01f9cde0a07c2372b416b
Arun Arumugasamy <arun@Aruns-MBP.lan>
2024-10-15 23:27:21 +0530

    Fix for blog card image cta click

--- blog/src/components/card/card.tsx
+++ blog/src/components/card/card.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image';
 import Link from 'next/link';
 
 export default function Card({title, imageUrl, ctaUrl}:{title: string, imageUrl: string, ctaUrl: string}){
-    return <div className={`w-full h-full flex flex-col hover:ease-in hover:-translate-y-1 duration-300 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700`}>
+    return <Link href={ctaUrl} className={`w-full h-full flex flex-col hover:ease-in hover:-translate-y-1 duration-300 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700`}>
         <Image
             src={imageUrl}
             width={200}
@@ -10,9 +10,9 @@ export default function Card({title, imageUrl, ctaUrl}:{title: string, imageUrl:
             className={`w-full h-3/5 contain-size object-fill`}
             alt={`${title} - image`}
             />
-        <Link className={`w-full flex flex-row h-2/5 p-2`} href={ctaUrl}>
+        <div className={`w-full flex flex-row h-2/5 p-2`}>
             <h3 className={`flex w-4/5`}>{title}</h3>
             <div className={`w-1/5 text-end align-bottom`}> <span className={`block mt-12 font-bold text-gray-500 `}> &gt;</span> </div>
-        </Link>
-    </div>
+        </div>
+    </Link>
 } 
