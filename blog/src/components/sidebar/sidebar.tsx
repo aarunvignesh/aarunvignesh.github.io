30efde042e8e6cb7c6b6955187ccf22e42189c38
Arun Arumugasamy <arun@Aruns-MBP.lan>
2024-10-15 22:49:15 +0530

    Replacing the a with next lint component

--- blog/src/components/sidebar/sidebar.tsx
+++ blog/src/components/sidebar/sidebar.tsx
@@ -1,6 +1,7 @@
 // Sidebar.js
 import React, {useState} from 'react';
 import {updateSidebarState} from './../../stores/sidebar';
+import Link from 'next/link';
 
 export const Sidebar = () => {
   let [isSidebarVisible, setSideBarToggle] = useState(updateSidebarState.getState());
@@ -19,13 +20,13 @@ export const Sidebar = () => {
         <h2 className="text-2xl font-bold">Navigations</h2>
         <div className={`flex flex-col mt-4 flex-wrap justify-evenly justify-around md:w-1/2 text-zinc-400`}>
                 <div className={`flex w-auto p-4`}>
-                    <a href="/profile">About me</a>
+                    <Link href="/profile">About me</Link>
                 </div>
                 <div className={`flex w-auto p-4`}>
-                    <a href="/">Blog</a>
+                    <Link href="/">Blog</Link>
                 </div>
                 <div className={`flex w-auto p-4`}>
-                    <a href="/search">Search</a>
+                    <Link href="/search">Search</Link>
                 </div>
         </div>
         <div className={`w-auto mt-auto mb-14`}>
